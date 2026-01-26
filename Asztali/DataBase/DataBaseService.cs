de12e2676a216e2e8d4e575e4b0dcb476c468bd2
TothMilan <tothmilan@turr.hu>
2026-01-26 12:09:55 +0100

    Összekötés

---
+++ Asztali/DataBase/DataBaseService.cs
@@ -0,0 +1,56 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asztali.DataBase
+{
+    internal class DataBaseService
+    {
+        private static string connectionString;
+        private static string table;
+        private static string query_parameters;
+
+
+        public static void DbConnectionCheck(string connectionString)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Sikeres csatlakozás");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No no");
+                Console.WriteLine(ex);
+            }
+        }
+
+
+
+        public static DataTable GetAllData(string tableName, string connectionString)
+        {
+            //kapcsolodas
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+            //kell egy parancs
+            using var command = new MySqlCommand("select * from " + tableName, connection);
+            //parancs feldolgozasa
+            using var reader = command.ExecuteReader();
+            //adatszerkezet letrehozasa
+            var dataTable = new DataTable();
+            //betoltjuk a lekert es feldolgozott adatokat
+            dataTable.Load(reader);
+            return dataTable;
+        }
+
+        
+    }
+}
--- Asztali/Program.cs
+++ Asztali/Program.cs
@@ -1,7 +1,41 @@
-﻿internal class Program
+﻿using Asztali.DataBase;
+using Asztali.Model;
+using System.Data;
+
+internal class Program
 {
+    public static readonly string conncetionString = "Server=localhost;Database=11a_foldrajz;User=root;";
+    public static DataBaseService dbservice = new DataBaseService();
+
+    //adattarolo
+    public static DataTable adatok = new DataTable();
+    public static List<Konyvtar> lista = new List<Konyvtar>();
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("A GITHUB EGY SZAR!");
+        Console.WriteLine("Professional munka zajlik...");
+
+    }
+    private static void Adatbetoltes(DataTable adatok)
+    {
+        foreach (DataRow o in adatok.Rows)
+        {
+            Konyvtar konyv = new Konyvtar();
+
+            
+
+            lista.Add(konyv);
+        }
+    }
+
+    private static void SelectFromTable(string tableName, string conncetionString)
+    {
+        adatok = DataBaseService.GetAllData(tableName, conncetionString);
+        Console.WriteLine("Adatok sikeresen szinkronizálva!");
+    }
+
+    private static void DbCheck(string conncetionString)
+    {
+        DataBaseService.DbConnectionCheck(conncetionString);
     }
 }

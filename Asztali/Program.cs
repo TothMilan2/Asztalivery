e549dff88b66596ff3e973d806b086eea95877aa
TothMilan <tothmilan@turr.hu>
2026-02-02 12:29:47 +0100

    Finishing up the remaining tasks

--- Asztali/Program.cs
+++ Asztali/Program.cs
@@ -16,23 +16,47 @@ internal class Program
         SelectFromTable("konyvek", conncetionString);
         Adatbetoltes(ref adatok);
 
+        Console.WriteLine("-------------------");
         Ketezertizenketto(lista);
+        Console.WriteLine("--------------------");
         MennyikonyvOsszTrue(lista);
+        Console.WriteLine("--------------------");
         Ezerkilencszazkilencvenkilenc(lista);
+        Console.WriteLine("-------------------");
+        OsszesKolcsonzott(lista);
+        Console.WriteLine("-------------------");
+        Konyvkereses(lista);
 
-        
+
+
+
     }
 
-    private static void Ezerkilencszazkilencvenkilenc(List<Konyvtar> lista)
+    private static void Konyvkereses(List<Konyvtar> lista)
     {
-        Console.WriteLine($"Könyvek, amik 1999 ben lettek kiadva:");
-        foreach (var i in lista)
+        Console.WriteLine("Melyik év adatait szeretnéd látni? ");
+        int be = Convert.ToInt32(Console.ReadLine());
+        foreach (var item in lista)
         {
-            if(i.Kiadasiev == 1999)
+            if (item.Kiadasiev == be)
             {
-                Console.WriteLine(i.Booknev);
+                Console.WriteLine($"\t{item.Booknev}, {item.Kivaneveve}, {item.Kiadasiev}, {item.Kolcsonzesideje}, {item.ISBN}");
             }
         }
+    }
+
+    private static void OsszesKolcsonzott(List<Konyvtar> lista)
+    {
+        int db = 0;
+
+        foreach (var item in lista)
+        {
+
+            db++;
+
+        }
+
+        Console.WriteLine($"Az összes kölcsönzött könyv: {db}");
     }
 
     private static void MennyikonyvOsszTrue(List<Konyvtar> lista)
@@ -43,46 +67,71 @@ internal class Program
             if (item.Kivaneveve == "true")
             {
                 db++;
-                Console.WriteLine($"Kivett könyvek összesen: {db}");
+
+
+            }
+
+        }
+        Console.WriteLine($"Kivett könyvek összesen: {db}");
+
+
+
+    }
+    private static void Ezerkilencszazkilencvenkilenc(List<Konyvtar> lista)
+    {
+        Console.WriteLine($"Könyvek, amik 1999 ben lettek kiadva:");
+        foreach (var i in lista)
+        {
+            if (i.Kiadasiev == 1999)
+            {
+                Console.WriteLine($" \t{i.Booknev}");
             }
         }
     }
 
-        private static void Ketezertizenketto(List<Konyvtar> lista)
+   
+
+    private static void Ketezertizenketto(List<Konyvtar> lista)
+    {
+        Console.WriteLine("2012-ben kiadott könyvek: ");
+        foreach (var item in lista)
         {
-            foreach (var item in lista)
+            if (item.Kiadasiev == 2012)
             {
-                if (item.Kiadasiev == 2012)
-                {
-                    Console.WriteLine(item.Booknev);
-                }
+                Console.WriteLine($" \t{item.Booknev}");
             }
         }
-        private static void Adatbetoltes(ref DataTable adatok)
+    }
+    private static void Adatbetoltes(ref DataTable adatok)
+    {
+        foreach (DataRow o in adatok.Rows)
         {
-            foreach (DataRow o in adatok.Rows)
-            {
-                Konyvtar konyv = new Konyvtar();
+            Konyvtar konyv = new Konyvtar();
 
-                konyv.BookID = o.Field<int>(0);
-                konyv.Booknev = o.Field<string>(1);
-                konyv.Kivaneveve = o.Field<string>(2);
-                konyv.Kiadasiev = o.Field<int>(3);
-                konyv.Kolcsonzesideje = o.Field<string>(4);
-                konyv.ISBN = o.Field<long>(5);
+            konyv.BookID = o.Field<int>(0);
+            konyv.Booknev = o.Field<string>(1);
+            konyv.Kivaneveve = o.Field<string>(2);
+            konyv.Kiadasiev = o.Field<int>(3);
+            konyv.Kolcsonzesideje = o.Field<string>(4);
+            konyv.ISBN = o.Field<long>(5);
 
-                lista.Add(konyv);
-            }
+            lista.Add(konyv);
         }
+    }
 
-        private static void SelectFromTable(string tableName, string conncetionString)
-        {
-            adatok = DataBaseService.GetAllData(tableName, conncetionString);
-            Console.WriteLine("Adatok sikeresen szinkronizálva!");
-        }
+    private static void SelectFromTable(string tableName, string conncetionString)
+    {
+        adatok = DataBaseService.GetAllData(tableName, conncetionString);
+        Console.WriteLine("Adatok sikeresen szinkronizálva!");
+    }
 
-        private static void DbCheck(string conncetionString)
-        {
-            DataBaseService.DbConnectionCheck(conncetionString);
-        }
-    } 
+    private static void DbCheck(string conncetionString)
+    {
+        DataBaseService.DbConnectionCheck(conncetionString);
+    }
+} 
+
+
+
+
+    

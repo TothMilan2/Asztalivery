dd6f2a81685d132ea43af76bbe22f351ee87cf04
KissNorbert1203 <kissnorbert@turr.hu>
2026-02-03 09:16:59 +0100

    2001_konyvek

---
+++ Asztali/Model/siker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asztali.Model
+{
+    internal class siker
+    {
+        private string _konyvnev;
+
+        public string Konyvnev { get => _konyvnev; set => _konyvnev = value; }
+
+        public siker(string konyvnev)
+        {
+            Konyvnev = konyvnev;
+        }
+
+        public siker()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"2001 ben kiadott könyvek: {Konyvnev}";
+        }
+    }
+}
